7d6655eed7c931743247eeb8fb52522eecf258fd
Monica Dinculescu <noms@google.com>
2017-10-04 15:55:44 +0200

    fix news-article

--- src/news-article.js
+++ src/news-article.js
@@ -1,25 +1,15 @@
-<!--
-@license
-Copyright (c) 2016 The Polymer Project Authors. All rights reserved.
-This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
-The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
-The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
-Code distributed by Google as part of the polymer project is also
-subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
--->
+import { Element } from '../node_modules/@polymer/polymer/polymer-element.js';
+import '../node_modules/@polymer/app-route/app-route.js';
+import '../node_modules/@polymer/iron-icon/iron-icon.js';
+import './news-article-cover.js';
+import './news-iframe.js';
+import './news-side-list.js';
+import { afterNextRender } from '../node_modules/@polymer/polymer/lib/utils/render-status.js';
+import { scroll } from '../node_modules/@polymer/app-layout/helpers/helpers.js';
 
-<script type="module" src="../node_modules/@polymer/polymer/polymer-element.js"></script>
-<script type="module" src="../node_modules/@polymer/app-route/app-route.js"></script>
-<script type="module" src="../node_modules/@polymer/iron-icon/iron-icon.js"></script>
-
-<script type="module" src="./news-article-cover.js"></script>
-<script type="module" src="./news-iframe.js"></script>
-<script type="module" src="./news-side-list.js"></script>
-
-<dom-module id="news-article">
-
-  <template>
-
+class NewsArticle extends Element {
+  static get template() {
+    return `
     <style>
       :host {
         display: block;
@@ -193,22 +183,8 @@ subject to an additional IP rights grant found at http://polymer.github.io/PATEN
     <news-network-warning
         hidden$="[[!failure]]"
         offline="[[offline]]"
-        on-try-reconnect="_tryReconnect"></news-network-warning>
-
-  </template>
-
-  <script type="module">
-import { Element } from '../node_modules/@polymer/polymer/polymer-element.js';
-import '../node_modules/@polymer/app-route/app-route.js';
-import '../node_modules/@polymer/iron-icon/iron-icon.js';
-import './news-article-cover.js';
-import './news-iframe.js';
-import './news-side-list.js';
-import { afterNextRender } from '../node_modules/@polymer/polymer/lib/utils/render-status.js';
-import { scroll } from '../node_modules/@polymer/app-layout/helpers/helpers.js';
-
-class NewsArticle extends Element {
-
+        on-try-reconnect="_tryReconnect"></news-network-warning>`;
+  }
   static get is() { return 'news-article'; }
 
   static get properties() { return {
@@ -460,6 +436,3 @@ class NewsArticle extends Element {
 }
 
 customElements.define(NewsArticle.is, NewsArticle);
-</script>
-
-</dom-module>
